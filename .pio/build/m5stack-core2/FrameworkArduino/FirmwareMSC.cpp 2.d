3e25e8443b0f1eab1186f4ea6f1f905c8928263d
vtallen <vtallen@protonmail.com>
2023-04-03 21:17:58 -0400

    Inital commit

---
+++ .pio/build/m5stack-core2/FrameworkArduino/FirmwareMSC.cpp 2.d
@@ -0,0 +1,5 @@
+.pio/build/m5stack-core2/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/m5stack-core2/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/m5stack-core2/FrameworkArduino/cbuf.cpp.o: \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/vincentallen/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
